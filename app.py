cf2614dc846077ca1616c71bca1a7edf24a94b1a
Rochan543 <premrochan@gmail.com>
2025-06-08 10:26:20 +0530

    Initial commit: Flask To-Do App

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+# Simple in-memory list for tasks
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    if task:
+        tasks.append(task)
+    return redirect('/')
+
+@app.route('/delete/<int:index>')
+def delete(index):
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
